5103f06a77a216d35c1d2e4f2cc202b36f1e81eb
saumb <gandhisaumya8@gmail.com>
2021-12-21 00:08:47 +0530

    sweep fix

--- trainer.py
+++ trainer.py
@@ -6,4 +6,4 @@ if __name__=="__main__":
     parser = ArgumentParser()
     parser.add_argument("sweep_id")
     args = parser.parse_args()
-    wandb.agent(args['sweep_id'], function=train_with_wandb, project='absa-enterpret', entity='saumb7800')
+    wandb.agent(args.sweep_id, function=train_with_wandb, project='absa-enterpret', entity='saumb7800')
